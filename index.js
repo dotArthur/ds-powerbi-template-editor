cc03f290e640d4f001da4b457dce2ebb5575570b
Arthur Andersen <a.andersen@dotsource.de>
2023-09-18 18:44:10 +0200

    deploy ColorPicker to index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+import React, {useEffect, useState} from 'react'
+import ReactDom from "react-dom"
+import ColorPicker from "./ColorPicker"
+
+function App() {
+  ReactDom.render(ColorPicker(),document.getElementById('root'))
+  return (
+    <div>
+      <h1>Power BI Theme Editor</h1>
+    </div>
+  );
+}
+
+export default App
+
 const express = require('express')
 const path = require('path')
 
