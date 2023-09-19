bb5198f972222472a4ee27615e85bfe8380f6c82
Arthur Andersen <a.andersen@dotsource.de>
2023-09-19 10:41:01 +0200

    ColorPicker => project ordner

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import ReactDom from "react-dom"
-import ColorPicker from "./React-Components/ColorPicker"
+import ColorPicker from "./ColorPicker"
 
 function App() {
   return (
