246f9c49e774e3ef374f29fa87c377429cff096b
Arthur Andersen <a.andersen@dotsource.de>
2023-09-19 09:19:18 +0200

    impliment Colorpicker (more rights)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import ReactDom from "react-dom"
-import ColorPicker from "./ColorPicker"
+import ColorPicker from "./React-Components/ColorPicker"
 
 function App() {
   ReactDom.render(ColorPicker(),document.getElementById('root'))
