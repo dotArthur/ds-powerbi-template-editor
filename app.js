e888cd8f3abaf2245e58e3094e161f02554a66e2
dotArthur <a.andersen@dotSource.de>
2023-09-28 16:37:43 +0200

    try some files out of other folder

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const app = express();
 
 // It listens to HTTP get request.
 // Here it listens to the root i.e '/'
+const container = document.getElementById('root')
+const root = createRoot(container)
+
 app.get("/", (req, res) => {
 
 // Using send function we send response to the client Here we are sending html
-res.send("<h1> Hello World </h1>");
+res.send("root.render(<App />);");
 });
 
 // It configures the system to listen to port 3000. Any number can be given instead of 3000, the only condition is that no other server
