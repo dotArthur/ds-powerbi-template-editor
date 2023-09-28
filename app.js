e0eb88524afaadb97f8263a934573c48b6b53cba
dotArthur <a.andersen@dotSource.de>
2023-09-28 17:22:04 +0200

    same as last commit

--- app.js
+++ app.js
@@ -9,20 +9,8 @@ const app = express();
 app.get("/", (req, res) => {
 
 // Using send function we send response to the client Here we are sending html
-res.write("<h1> Hello World </h1>");
-("<button>Set JSON</button>");
-("<button>Set JSON</button>");
-("<button>Set JSON</button>");
-("<button>Set JSON</button>");
-("<button>Set JSON</button>");
-("<button>Set JSON</button>");
-("<button>Set JSON</button>");
-("<button>Set JSON</button>");
-("<button>Set JSON</button>");
-("<button>Set JSON</button>");
-("<button>Set JSON</button>");
-("<button>Set JSON</button>");
-("<button>Set JSON</button>");
+res.write("<h1> Hello World </h1>")
+("<button>Set JSON</button>")
 res.end()
 });
 
