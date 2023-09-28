131955f08e96347e8d4d3fc17c016ef922504205
dotArthur <a.andersen@dotSource.de>
2023-09-28 17:16:08 +0200

    ; after every line

--- app.js
+++ app.js
@@ -9,21 +9,21 @@ const app = express();
 app.get("/", (req, res) => {
 
 // Using send function we send response to the client Here we are sending html
-res.send("<h1> Hello World </h1>")
-("<button>Set JSON</button>")
-("<button>Set JSON</button>")
-("<button>Set JSON</button>")
-("<button>Set JSON</button>")
-("<button>Set JSON</button>")
-("<button>Set JSON</button>")
-("<button>Set JSON</button>")
-("<button>Set JSON</button>")
-("<button>Set JSON</button>")
-("<button>Set JSON</button>")
-("<button>Set JSON</button>")
-("<button>Set JSON</button>")
-("<button>Set JSON</button>")
-;;});
+res.send("<h1> Hello World </h1>");
+("<button>Set JSON</button>");
+("<button>Set JSON</button>");
+("<button>Set JSON</button>");
+("<button>Set JSON</button>");
+("<button>Set JSON</button>");
+("<button>Set JSON</button>");
+("<button>Set JSON</button>");
+("<button>Set JSON</button>");
+("<button>Set JSON</button>");
+("<button>Set JSON</button>");
+("<button>Set JSON</button>");
+("<button>Set JSON</button>");
+("<button>Set JSON</button>");
+});
 
 
 
