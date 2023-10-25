dd7e676ed60630951deb508989ed63ee19b6cc90
dotArthur <a.andersen@dotSource.de>
2023-10-25 15:20:45 +0200

    add button styling

--- src/App.js
+++ src/App.js
@@ -2,26 +2,27 @@ import './App.css';
 import { JSONEditor } from "react-schema-based-json-editor";
 import { DATA } from './PowerBISchema';
 
-
-function App() {
   //I need a function called updateValue that will update the value of the JSONEditor
   const updateValue = (value) => {
     //update the initial value of the JSONEditor
     console.log(value);
     };
+
+function App() {
   return (
     <div className="App">
-        <JSONEditor
-        schema={DATA}
-        initialValue={{ 
-          "name": "Waveform", 
-          "dataColors": ["#31B6FD", "#4584D3", "#5BD078", "#A5D028", "#F5C040", "#05E0DB", "#3153FD", "#4C45D3", "#5BD0B0", "#54D028", "#D0F540", "#057BE0"],
-          "background":"#FFFFFF",
-          "foreground": "#4584D3",
-          "tableAccent": "#31B6FD"
-      }}
-        updateValue={updateValue}
-        ></JSONEditor>
+      <button>Hier Klicken!!!</button>
+            <JSONEditor
+            schema={DATA}
+            initialValue={{ 
+            "name": "Waveform", 
+            "dataColors": ["#31B6FD", "#4584D3", "#5BD078", "#A5D028", "#F5C040", "#05E0DB", "#3153FD", "#4C45D3", "#5BD0B0", "#54D028", "#D0F540", "#057BE0"],
+            "background":"#FFFFFF",
+            "foreground": "#4584D3",
+            "tableAccent": "#31B6FD"
+            }}
+            updateValue={updateValue}
+            ></JSONEditor>
     </div>
   );
 }
