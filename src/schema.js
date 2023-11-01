23a5492ffb19fd14d07b395815dd9a3181d98269
dotArthur <a.andersen@dotSource.de>
2023-11-01 10:59:37 +0100

    ..

--- src/schema.js
+++ src/schema.js
@@ -1,30 +1,30 @@
-// export var schema ={
-//     "type": "object",
-//     "properties": {
-//       "chartType": {
-//         "type": "string",
-//         "title": "Chart Type"
-//       },
-//       "barDirection": {
-//         "type": "string",
-//         "title": "Bar Direction"
-//       },
-//       "barStyle": {
-//         "type": "object",
-//         "title": "Bar Style",
-//         "properties": {
-//           "fill": {
-//             "title": "Fill",
-//             "type": "string",
-//             "format": "color"
-//           },
-//           "stroke": {
-//             "title": "Stroke",
-//             "type": "string",
-//             "format": "color"
-//         }
-//       }
-//     }
-//   },
-//   "required": ["chartType", "barDirection", "barStyle"]
-// }
+export var schema ={
+    "type": "object",
+    "properties": {
+      "chartType": {
+        "type": "string",
+        "title": "Chart Type"
+      },
+      "barDirection": {
+        "type": "string",
+        "title": "Bar Direction"
+      },
+      "barStyle": {
+        "type": "object",
+        "title": "Bar Style",
+        "properties": {
+          "fill": {
+            "title": "Fill",
+            "type": "string",
+            "format": "color"
+          },
+          "stroke": {
+            "title": "Stroke",
+            "type": "string",
+            "format": "color"
+        }
+      }
+    }
+  },
+  "required": ["chartType", "barDirection", "barStyle"]
+}
