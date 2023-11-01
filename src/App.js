cb5bc77f57d2cc5df16d3022360b00e0ab568d43
dotArthur <a.andersen@dotSource.de>
2023-11-01 11:13:12 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 import { JSONEditor } from "react-schema-based-json-editor";
-import { schema } from './schema';
+import { schema } from 'https://cloud-cube-eu2.s3.amazonaws.com/tgead1omqd79/public/schema.js';
 
 function App() {
   const [initialValue, setValue] = useState({});
