1eaf1c3c7d52018c213b7f5d966768d31e6e52e2
dotArthur <a.andersen@dotSource.de>
2023-11-07 16:38:57 +0100

    https://cloud-cube-eu2.s3.amazonaws.com/zqke2rj18o5a/public/schema.json as fetch url

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   };
 
   async function getData() {
-    const response = await fetch("https://cloud-cube-eu2.s3.amazonaws.com/zqke2rj18o5a/public/API/schema.json");
+    const response = await fetch("https://cloud-cube-eu2.s3.amazonaws.com/zqke2rj18o5a/public/schema.json");
     setData(await response.json());
     return Data;
   }
