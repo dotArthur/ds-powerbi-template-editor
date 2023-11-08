f406751cc5bb44b6a2c4a8a0cdbf2d91cee159e0
dotArthur <a.andersen@dotSource.de>
2023-11-08 16:19:59 +0100

    cut out webVitals

--- src/index.js
+++ src/index.js
@@ -2,20 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-// import {DATA} from './PowerBISchema+';
 
-
-// var daten = DATA;
-// console.log (daten);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log());
