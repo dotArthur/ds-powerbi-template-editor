0a3c01355e84ba355131ed0a68f90f5111fb3557
dotArthur <a.andersen@dotSource.de>
2023-11-13 16:31:04 +0100

    add loader while fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { JSONEditor } from '@json-editor/json-editor';
 
 function App() {
   const editorHolder = useRef(null);
+  const [loading, setLoading] = useState(true);
+  const [dataName, setDataName] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch("https://cloud-cube-eu2.s3.amazonaws.com/vqwb9svw2seg/public/data.json")
+      const response = await fetch("https://cloud-cube-eu2.s3.amazonaws.com/vqwb9svw2seg/public/data.json");
       const data = await response.json();
+      setDataName(data.name);
+      setLoading(false);
 
       if (editorHolder.current) {
         const editor = new JSONEditor(editorHolder.current, {
@@ -21,30 +25,34 @@ function App() {
           compact: true
         });
 
-        editor.on('ready', function() {
+        editor.on('ready', function () {
           editor.setValue(data);
         });
       }
     };
-    
+
     fetchData();
   }, []);
-    const downloadJson = () => {
-      const json = JSON.stringify(editorHolder.current.editor.getValue(), null, 2);
-      const blob = new Blob([json], { type: 'application/json' });
-      const url = URL.createObjectURL(blob);
-      const anchorElement = document.createElement('a');
-      anchorElement.href = url;
-      anchorElement.download = data.name +'.json';
-      anchorElement.click();
-      URL.revokeObjectURL(url);
-    };
 
+  const downloadJson = () => {
+    const json = JSON.stringify(editorHolder.current.editor.getValue(), null, 2);
+    const blob = new Blob([json], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = dataName + '.json';
+    anchorElement.click();
+    URL.revokeObjectURL(url);
+  };
+
+  if (loading) {
+    return null;
+  }
 
   return (
     <>
-    <div ref={editorHolder}></div>
-    <button onClick={downloadJson}>Download JSON</button>
+      <div ref={editorHolder}></div>
+      <button onClick={downloadJson}>Download JSON</button>
     </>
   );
 }
