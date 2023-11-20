b90be1cb807c2fa831e4cd7e8fff3834786c9d87
dotArthur <a.andersen@dotSource.de>
2023-11-20 16:42:25 +0100

    .

--- src/barChartComponent.jsx
+++ src/barChartComponent.jsx
@@ -5,6 +5,7 @@ import * as Schema from '../PowerBITheme.json'
 
 export default function BarChart() {
   const editorHolder = useRef(null);
+  const [editorVal, seteditorVal] = useState({});
 
   useEffect(() => {
     const editor = new JSONEditor(editorHolder.current, {
@@ -18,38 +19,33 @@ export default function BarChart() {
       display_required_only: true,
       disable_collapse: true,
       form_name_root: 'ds-powerbi-template-editor',
+      no_additional_properties: true,
     });
+
+     const JsonDownloader = document.getElementById('downloadButton');
+     JsonDownloader.addEventListener('click', downloadJson);
 
     editor.on('ready', function () {
-      editor.setValue({});
-      // Get the Object Properties button
-      const objectPropertiesButton = document.querySelector('.json-editor-btn-edit_properties');
-      // Add a 'click' event listener to the button
-      objectPropertiesButton.addEventListener('click', function () {
-        // Get all checkbox elements within the parent element
-        const checkboxes = document.querySelectorAll('.form-control input[type=checkbox]');
-        // Loop over the checkboxes
-        for (let i = 0; i < checkboxes.length; i++) {
-          // Set the checked attribute to true
-          checkboxes[i].checked = true;
-          // Create a new 'change' event
-          var event = new Event('change');
-          // Dispatch the event
-          checkboxes[i].dispatchEvent(event);
-        }
-      });
+      // editor.setValue({});
     });
 
+    editor.on('change', function () {
+      let value = editor.getValue()
+      seteditorVal(value);
+      console.log(value);
+      console.log(editorVal);
+    });
   }, []);
   
   const downloadJson = () => {
-    if (editorHolder.current && editorHolder.current.editor) {
-      const json = JSON.stringify(editorHolder.current.editor.getValue(), null, 2);
+    if (editorHolder.current) {
+      const editorValue = editorVal;
+      const json = JSON.stringify(editorValue, null, 2);
       const blob = new Blob([json], { type: 'application/json' });
       const url = URL.createObjectURL(blob);
       const anchorElement = document.createElement('a');
       anchorElement.href = url;
-      anchorElement.download = editorHolder.current.editor.getValue(name) + '.json';
+      anchorElement.download ='.json';
       anchorElement.click();
       URL.revokeObjectURL(url);
     }
@@ -58,7 +54,6 @@ export default function BarChart() {
   return (
     <>
       <div ref={editorHolder}></div>
-      <button onClick={() => downloadJson}>Download JSON</button>    
       </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './main.css'
 import BarChart from './barChartComponent';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BarChart />
+    <div className='downloadButtonDiv'>
+      <button type='button' id='downloadButton'>Download JSON</button>
+    </div>
   </React.StrictMode>
 )
